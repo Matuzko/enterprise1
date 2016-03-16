4b1aca72bbfb91f6f82b901f43d2f72f818b8462
Dmitry <matuzkodm@gmail.com>
2016-03-16 13:55:29 +0200

    version 1.0

--- CollectionsBase.java
+++ CollectionsBase.java
@@ -1,7 +1,86 @@
 package enterprise1;
 
-/**
- * Created by Дима on 16.03.2016.
- */
+
+import java.util.ArrayList;
+import java.util.HashSet;
+import java.util.LinkedList;
+import java.util.TreeSet;
+
 public class CollectionsBase {
+    ArrayList arrayList = new ArrayList();
+    LinkedList linkedList = new LinkedList();
+    HashSet hashSet = new HashSet();
+    TreeSet treeSet = new TreeSet();
+
+    public CollectionsBase(int size) {
+        CollectionsActs.fill(arrayList, size);
+        CollectionsActs.fill(linkedList, size);
+        CollectionsActs.fill(hashSet, size);
+        CollectionsActs.fill(treeSet, size);
+    }
+
+    public ArrayList getArrayList() {
+        return arrayList;
+    }
+
+    public void setArrayList(ArrayList arrayList) {
+        this.arrayList = arrayList;
+    }
+
+    public LinkedList getLinkedList() {
+        return linkedList;
+    }
+
+    public void setLinkedList(LinkedList linkedList) {
+        this.linkedList = linkedList;
+    }
+
+    public HashSet getHashSet() {
+        return hashSet;
+    }
+
+    public void setHashSet(HashSet hashSet) {
+        this.hashSet = hashSet;
+    }
+
+    public TreeSet getTreeSet() {
+        return treeSet;
+    }
+
+    public void setTreeSet(TreeSet treeSet) {
+        this.treeSet = treeSet;
+    }
+
+    public String addEfectivity(int numberOfChanges) {
+        long arrayListAddEffectivity = CollectionsActs.addEffectivityCounter(this.arrayList, numberOfChanges);
+        long linkedListAddEffectivity = CollectionsActs.addEffectivityCounter(this.linkedList, numberOfChanges);
+        long hashSetAddEffectivity = CollectionsActs.addEffectivityCounter(this.hashSet, numberOfChanges);
+        long treeSetAddEffectivity = CollectionsActs.addEffectivityCounter(this.treeSet, numberOfChanges);
+        String resultAddEffectivity = "ArrayList size: " + arrayList.size() + " has added " + numberOfChanges + " elements in average for " + arrayListAddEffectivity + " nanoseconds\n"
+                + "LinkedList size: " + linkedList.size() + " has added " + numberOfChanges + " elements in average for " + linkedListAddEffectivity + " nanoseconds\n"
+                + "HashSet size: " + hashSet.size() + " has added " + numberOfChanges + " elements in average for " + hashSetAddEffectivity + " nanoseconds\n"
+                + "TreeSet size: " + treeSet.size() + " has added " + numberOfChanges + " elements in average for " + treeSetAddEffectivity + " nanoseconds\n";
+        return resultAddEffectivity;
+
+    }
+
+    public String getEffectivity(int numberOfChanges) {
+        long arrayListGetEffectivity = CollectionsActs.getEffectivityCounter(this.arrayList, numberOfChanges);
+        long linkedListGetEffectivity = CollectionsActs.getEffectivityCounter(this.linkedList, numberOfChanges);
+        String resultGetEffectivity = "ArrayList size: " + this.arrayList.size() + " has gotten " + numberOfChanges + " elements in average for " + arrayListGetEffectivity + " nanoseconds\n"
+                + "LinkedList size: " + this.linkedList.size() + " has gotten " + numberOfChanges + " elements in average for " + linkedListGetEffectivity + " nanoseconds\n";
+        return resultGetEffectivity;
+    }
+
+    public String removeEffectivity(int numberOfChanges) {
+        long arrayListRemoveEffectivity = CollectionsActs.removeEffectivityCounter(this.arrayList, numberOfChanges);
+        long linkedListRemoveEffectivity = CollectionsActs.removeEffectivityCounter(this.linkedList, numberOfChanges);
+        long hashSetRemoveEffectivity = CollectionsActs.removeEffectivityCounter(this.hashSet, numberOfChanges);
+        long treeSetRemoveEffectivity = CollectionsActs.removeEffectivityCounter(this.treeSet, numberOfChanges);
+        String resultRemoveEffectivity = "ArrayList size: " + arrayList.size() + " has removed " + numberOfChanges + " elements in average for " + arrayListRemoveEffectivity + " nanoseconds\n"
+                + "LinkedList size: " + linkedList.size() + " has removed " + numberOfChanges + " elements in average for " + linkedListRemoveEffectivity + " nanoseconds\n"
+                + "HashSet size: " + hashSet.size() + " has removed " + numberOfChanges + " elements in average for " + hashSetRemoveEffectivity + " nanoseconds\n"
+                + "TreeSet size: " + treeSet.size() + " has removed " + numberOfChanges + " elements in average for " + treeSetRemoveEffectivity + " nanoseconds\n";
+        return resultRemoveEffectivity;
+    }
 }
--- Main.java
+++ Main.java
@@ -1,18 +1,47 @@
 package enterprise1;
 
+import java.io.FileWriter;
+import java.io.IOException;
 import java.util.*;
 
 public class Main {
-    public static void main(String[] args) {
-        ArrayList arrayList = new ArrayList<>();
+    public static void main(String[] args) throws IOException {
+        FileWriter logWriter = null;
+                
+        try {
+            logWriter = new FileWriter("results.txt");
 
+            CollectionsBase collectionsBase10000 = new CollectionsBase(10000);
+            CollectionsBase collectionsBase100000 = new CollectionsBase(100000);
+            CollectionsBase collectionsBase1000000 = new CollectionsBase(1000000);
 
+            logWriter.write(collectionsBase10000.addEfectivity(100));
+            logWriter.write("\n" + "\n");
+            logWriter.write(collectionsBase100000.addEfectivity(100));
+            logWriter.write("\n" + "\n");
+            logWriter.write(collectionsBase1000000.addEfectivity(100));
+            logWriter.write("\n" + "\n");
 
-        System.out.println(CollectionEffectivity.addEffectivity(arrayList, 10000));
-        System.out.println(CollectionEffectivity.addEffectivity(arrayList, 100000));
-        System.out.println(CollectionEffectivity.addEffectivity(arrayList, 1000000));
-        System.out.println(CollectionEffectivity.addEffectivity(arrayList, 10000000));
+            logWriter.write(collectionsBase10000.getEffectivity(100));
+            logWriter.write("\n" + "\n");
+            logWriter.write(collectionsBase100000.getEffectivity(100));
+            logWriter.write("\n" + "\n");
+            logWriter.write(collectionsBase1000000.getEffectivity(100));
+            logWriter.write("\n" + "\n");
 
+            logWriter.write(collectionsBase10000.removeEffectivity(100));
+            logWriter.write("\n" + "\n");
+            logWriter.write(collectionsBase100000.removeEffectivity(100));
+            logWriter.write("\n" + "\n");
+            logWriter.write(collectionsBase1000000.removeEffectivity(100));
+            logWriter.write("\n" + "\n");
 
+        } catch (IOException ex) {
+            ex.printStackTrace();
+        } finally {
+            if (logWriter != null) {
+                logWriter.close();
+            }
+        }
     }
 }
