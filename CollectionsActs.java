f4190ce31b72288e72f15e6f5913e36549548bf0
Dmitry <matuzkodm@gmail.com>
2016-03-16 13:51:15 +0200

    beta version 1.0

---
+++ CollectionsActs.java
@@ -0,0 +1,7 @@
+package enterprise1;
+
+/**
+ * Created by Дима on 15.03.2016.
+ */
+public class CollectionsActs {
+}
---
+++ CollectionsBase.java
@@ -0,0 +1,7 @@
+package enterprise1;
+
+/**
+ * Created by Дима on 16.03.2016.
+ */
+public class CollectionsBase {
+}
